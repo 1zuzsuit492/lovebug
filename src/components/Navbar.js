0fcbe5eb803d0f81a8d8c41842fc9018db1d47de
Melissa Lopez <melissalopez@pursuit.org>
2022-08-10 18:26:21 -0400

    time to troubleshoot

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,36 @@
+import { Link } from "react-router-dom"
+
 export default function Navbar() {
     return (
-        <div className="navbar container">
-        <a href="#" className="logo">LoveBug Bakery</a>
-            <div className="links">
-                <a href="#">About Us</a>
-                <a href="#">Goodies</a>
-                <a href="#">Contact Us</a>
-            </div>
-        </div>
+        // <div className="navbar container">
+        // <a href="#" className="logo">LoveBug Bakery</a>
+        //     <div className="links">
+        //         <a href="#">About Us</a>
+        //         <a href="#">Goodies</a>
+        //         <a href="#">Contact Us</a>
+        //     </div>
+        // </div>
+<nav className='NavBar'>
+<ul>
+<div className="logo">
+  <Link to='/'> LoveBug Bakery </Link>
+  </div>
+</ul>
+<div className="links">
+<ul>
+  <Link to="/about">About Us</Link>
+</ul>
+  
+  <ul>
+  <Link to="/goodies">Goodies</Link>
+  </ul>
+
+  <ul>
+  <Link to="/contact">Contact Us</Link>
+  </ul>
+  </div>
+
+</nav>
+
     )
 }
