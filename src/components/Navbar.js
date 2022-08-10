37f80a156e7e9a58c97f93d06a5e927c498348b0
Melissa Lopez <melissalopez@pursuit.org>
2022-08-10 18:15:49 -0400

    somewhat of a navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
-export default function Navbar () {
+export default function Navbar() {
     return (
-        <div className="navbar">
-
+        <div className="navbar container">
+        <a href="#" className="logo">LoveBug Bakery</a>
+            <div className="links">
+                <a href="#">About Us</a>
+                <a href="#">Goodies</a>
+                <a href="#">Contact Us</a>
+            </div>
         </div>
     )
 }
