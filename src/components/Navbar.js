80e8602642a0713c7bcb26acaaa0fcbef6b87271
Melissa Lopez <melissalopez@pursuit.org>
2022-08-10 18:37:34 -0400

    quick fix to navbar with react links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,33 @@ import { Link } from "react-router-dom"
 
 export default function Navbar() {
     return (
+        <>
+            <nav className='Navbar'>
+                <ul>
+                    <div className="logo">
+                        <Link to='/'>LoveBug Bakery</Link>
+                    </div>
+                </ul>
+                <div className="links">
+                    <ul>
+                        <Link to="/about">About Us</Link>
+                    </ul>
+
+                    <ul>
+                        <Link to="/goodies">Goodies</Link>
+                    </ul>
+
+                    <ul>
+                        <Link to="/contact">Contact Us</Link>
+                    </ul>
+                </div>
+
+            </nav>
+        </>
+    )
+}
+
+
         // <div className="navbar container">
         // <a href="#" className="logo">LoveBug Bakery</a>
         //     <div className="links">
@@ -10,27 +37,3 @@ export default function Navbar() {
         //         <a href="#">Contact Us</a>
         //     </div>
         // </div>
-<nav className='NavBar'>
-<ul>
-<div className="logo">
-  <Link to='/'> LoveBug Bakery </Link>
-  </div>
-</ul>
-<div className="links">
-<ul>
-  <Link to="/about">About Us</Link>
-</ul>
-  
-  <ul>
-  <Link to="/goodies">Goodies</Link>
-  </ul>
-
-  <ul>
-  <Link to="/contact">Contact Us</Link>
-  </ul>
-  </div>
-
-</nav>
-
-    )
-}
