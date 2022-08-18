e3e4f027ec1d984fa94f2205276b817cd4b89760
Melissa Lopez <melissalopez@pursuit.org>
2022-08-18 13:18:26 -0400

    reseeded

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -1,10 +1,10 @@
 INSERT INTO products (name, description, price, image, category) VALUES
-('Macbook pro 16inch', 'Used 16inch 256gb storage, 8gb of ram', '490','https://i.imgur.com/D9qeLwU.jpg', 'electronics') ,
-('Computer Desk 24x12', 'Used for about a year with some wear', '49', 'https://i.imgur.com/Qyf1JTA.jpg', 'furniture'),
-('Polo Sweater', 'Gently Used Ralph Lauren', '21','https://i.imgur.com/00dkEcv.jpg', 'clothing'),
-('Racing Chair', 'GTR good condition', '70', 'https://i.imgur.com/rBkPgp8.jpg', 'furniture'),
-('Prom Dress', 'Red, used once', '100', 'https://i.imgur.com/8pUB4AN.png', 'clothing'),
-('PlayStation 5', 'Has plenty of digital games inside!', '100','https://i.imgur.com/KR7Rz17.jpg', 'electronics'),
-('Ludlow Suit Jacket', 'Unworn', '105', 'https://i.imgur.com/CGvwr33.jpg', 'clothing'),
-('Galaxy S7 plus tablet', '128GB', '420', 'https://i.imgur.com/Xk3fAy7.jpg', 'electronics'),
-('Bed Frame', 'Queen size', '100','https://i.imgur.com/4Vq99nV.jpg', 'furniture');
+('Vanilla Macaron', 'Made with real vanilla!', '2','https://picsum.photos/200', 'Macarons') ,
+('NY Style Cheesecake Slice', 'Made with love!', '4', 'https://picsum.photos/200', 'Cake'),
+('Chocolate Chip Cookie', 'Will melt in your mouth!', '2','https://picsum.photos/200', 'Cookies'),
+('Vanilla Cupcake', 'Always made fresh!', '3', 'https://picsum.photos/200', 'Cupcake'),
+('Chocolate Cake Slice', 'Gooey and chocolatey', '5', 'https://picsum.photos/200', 'Cake'),
+('Chocolate Macaron', 'Light and airy', '2','https://picsum.photos/200', 'Macarons'),
+('White Chocolate Cookie', 'Soooo soft!', '2', 'https://picsum.photos/200', 'Cookies'),
+('Chocolate Cupcake', 'Moist on the inside', '3', 'https://picsum.photos/200', 'Cupcake'),
+('Oatmeal Raisin Cookie', 'The classic.', '2','https://picsum.photos/200', 'Cookie');
